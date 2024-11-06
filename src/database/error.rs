31a34069e148f2c787b8674e74da083c40279382
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-11-06 21:22:33 +0900

    エラーメッセージを日本語に修正

--- src/database/error.rs
+++ src/database/error.rs
@@ -2,18 +2,15 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DbError {
-    #[error("Database error: {0}")]
+    #[error("データベースエラー: {0}")]
     Postgres(#[from] tokio_postgres::Error),
 
-    #[error("Connection pool error: {0}")]
+    #[error("接続プールエラー: {0}")]
     Pool(#[from] bb8::RunError<tokio_postgres::Error>),
 
-    #[error("JSON serialization error: {0}")]
+    #[error("JSONシリアル化エラー: {0}")]
     Serialization(#[from] serde_json::Error),
 
-    #[error("Database initialization error")]
+    #[error("データベース初期化エラー")]
     Initialization,
-
-    #[error("Other error: {0}")]
-    Other(String),
 }
