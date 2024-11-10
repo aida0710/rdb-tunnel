856e2b169beb4fb4bd08c8b49a8e04578266fb02
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-11-10 16:26:35 +0900

    未使用のnew関数を削除

--- src/packet/firewall/packet.rs
+++ src/packet/firewall/packet.rs
@@ -20,31 +20,6 @@ pub struct FirewallPacket {
 }
 
 impl FirewallPacket {
-    #[allow(clippy::too_many_arguments)]
-    pub fn new(
-        src_mac: MacAddr,
-        dst_mac: MacAddr,
-        ether_type: u16,
-        src_ip: IpAddr,
-        dst_ip: IpAddr,
-        ip_version: u8,
-        ip_protocol: u8,
-        src_port: u16,
-        dst_port: u16,
-    ) -> Self {
-        Self {
-            src_mac,
-            dst_mac,
-            ether_type,
-            src_ip,
-            dst_ip,
-            ip_version,
-            ip_protocol,
-            src_port,
-            dst_port,
-        }
-    }
-
     pub fn from_packet(packet: &crate::packet::Packet) -> Self {
         Self {
             src_mac: packet.src_mac.clone(),
