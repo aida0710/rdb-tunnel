983c37d724823ded634c6b4e24f11d197fd17407
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-11-26 18:02:44 +0900

    measure_time_asyncを取り入れ、何秒で処理されているのかを確認できるように

--- src/packet/reader/packet_sender.rs
+++ src/packet/reader/packet_sender.rs
@@ -1,6 +1,7 @@
 use crate::packet::reader::error::PacketReaderError;
 use crate::packet::Packet;
-use log::{debug, info, trace};
+use crate::utils::measure_time::measure_time_async;
+use log::trace;
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{self, NetworkInterface};
 
@@ -9,39 +10,40 @@ pub struct PacketSender;
 impl PacketSender {
     const MAX_PACKET_SIZE: usize = 1500;
 
-    pub fn send_packet(
+    pub async fn send_packet(
         interface: &NetworkInterface,
         packet: &Packet,
     ) -> Result<(), PacketReaderError> {
-        if packet.raw_packet.len() > Self::MAX_PACKET_SIZE {
-            return Err(PacketReaderError::SendError(format!(
-                "パケットサイズが制限を超えています: {} bytes (最大: {} bytes)",
-                packet.raw_packet.len(),
-                Self::MAX_PACKET_SIZE
-            )));
-        }
-
-        let (mut tx) = match datalink::channel(interface, Default::default()) {
-            Ok(Ethernet(tx, _)) => tx,
-            Ok(_) => return Err(PacketReaderError::UnsupportedChannelType),
-            Err(e) => return Err(PacketReaderError::NetworkError(e.to_string())),
-        };
-
-        match tx.send_to(&*packet.raw_packet, None) {
-            Some(Ok(_)) => {
-                trace!(
-                    "送信パケット詳細: EtherType: {}, 送信元IP: {}, 宛先IP: {}, 送信元ポート: {}, 宛先ポート: {}, IPプロトコル: {}, タイムスタンプ: {}",
-                    packet.ether_type, packet.src_ip, packet.dst_ip,
-                    packet.src_port.map_or("未設定".to_string(), |p| p.to_string()),
-                    packet.dst_port.map_or("未設定".to_string(), |p| p.to_string()),
-                    packet.ip_protocol, packet.timestamp
-                );
-                Ok(())
+        measure_time_async("パケット送信", async {
+            if packet.raw_packet.len() > Self::MAX_PACKET_SIZE {
+                return Err(PacketReaderError::SendError(format!(
+                    "パケットサイズが制限を超えています: {} bytes (最大: {} bytes)",
+                    packet.raw_packet.len(),
+                    Self::MAX_PACKET_SIZE
+                )));
             }
-            Some(Err(e)) => Err(PacketReaderError::SendError(e.to_string())),
-            None => Err(PacketReaderError::SendError(
-                "宛先が指定されていません".into(),
-            )),
-        }
+
+            let mut tx = match datalink::channel(interface, Default::default()) {
+                Ok(Ethernet(tx, _)) => tx,
+                Ok(_) => return Err(PacketReaderError::UnsupportedChannelType),
+                Err(e) => return Err(PacketReaderError::NetworkError(e.to_string())),
+            };
+
+            match tx.send_to(&*packet.raw_packet, None) {
+                Some(Ok(_)) => {
+                    trace!(
+                        "送信パケット詳細: EtherType: {}, 送信元IP: {}, 宛先IP: {}, 送信元ポート: {}, 宛先ポート: {}, IPプロトコル: {}, タイムスタンプ: {}",
+                        packet.ether_type, packet.src_ip, packet.dst_ip,
+                        packet.src_port.map_or("未設定".to_string(), |p| p.to_string()),
+                        packet.dst_port.map_or("未設定".to_string(), |p| p.to_string()),
+                        packet.ip_protocol, packet.timestamp
+                    );
+                    Ok(())
+                }
+                Some(Err(e)) => Err(PacketReaderError::SendError(e.to_string())),
+                None => Err(PacketReaderError::SendError("宛先が指定されていません".into())),
+            }
+        })
+        .await
     }
 }
