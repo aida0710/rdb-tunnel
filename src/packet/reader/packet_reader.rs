274c76b5bc215882cc1946e4f3e4b219a7a4744d
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-11-26 18:03:12 +0900

    不要なuse文を削除し、単純化

--- src/packet/reader/packet_reader.rs
+++ src/packet/reader/packet_reader.rs
@@ -1,28 +1,21 @@
 use crate::packet::reader::error::PacketReaderError;
 use crate::packet::reader::packet_sender::PacketSender;
 use crate::packet::repository::PacketRepository;
-use crate::packet::Packet;
-use log::{debug, error, info, trace};
-use pnet::datalink::NetworkInterface;
-use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
-use std::sync::Arc;
-use std::time::Duration;
 use futures::future::join_all;
-use tokio::sync::Mutex;
+use log::error;
+use pnet::datalink::NetworkInterface;
+use std::time::Duration;
 
 #[derive(Clone)]
 pub struct PacketReader {}
 
-use tokio::runtime::Runtime;
-
 impl PacketReader {
     pub async fn start(interface: NetworkInterface) -> Result<(), PacketReaderError> {
-
         while let Ok(packets) = PacketRepository::get_filtered_packets(false, None).await {
             let sends = packets.into_iter().map(|packet| {
                 let interface_clone = interface.clone();
                 tokio::spawn(async move {
-                    if let Err(e) = PacketSender::send_packet(&interface_clone, &packet) {
+                    if let Err(e) = PacketSender::send_packet(&interface_clone, &packet).await {
                         error!("パケットの送信に失敗しました: {:?}", e);
                     }
                 })
