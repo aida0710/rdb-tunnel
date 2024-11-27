74961ce3f041fea25d224598cff5db9b6360648c
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-11-27 14:31:53 +0900

    エラーハンドリングを追加してパケット取得失敗時にリトライする

--- src/packet/reader/packet_reader.rs
+++ src/packet/reader/packet_reader.rs
@@ -9,22 +9,27 @@ use std::time::Duration;
 #[derive(Clone)]
 pub struct PacketReader {}
 
-impl PacketReader {
-    pub async fn start(interface: NetworkInterface) -> Result<(), PacketReaderError> {
-        while let Ok(packets) = PacketRepository::get_filtered_packets(false, None).await {
-            let sends = packets.into_iter().map(|packet| {
-                let interface_clone = interface.clone();
-                tokio::spawn(async move {
-                    if let Err(e) = PacketSender::send_packet(&interface_clone, &packet).await {
-                        error!("パケットの送信に失敗しました: {:?}", e);
-                    }
-                })
-            });
+pub async fn start(interface: NetworkInterface) -> Result<(), PacketReaderError> {
+    loop {
+        match PacketRepository::get_filtered_packets(false, None).await {
+            Ok(packets) => {
+                let sends = packets.into_iter().map(|packet| {
+                    let interface_clone = interface.clone();
+                    tokio::spawn(async move {
+                        if let Err(e) = PacketSender::send_packet(&interface_clone, &packet).await {
+                            error!("パケットの送信に失敗しました: {:?}", e);
+                        }
+                    })
+                });
 
-            join_all(sends).await;
-            tokio::time::sleep(Duration::from_millis(10)).await;
+                join_all(sends).await;
+                tokio::time::sleep(Duration::from_millis(10)).await;
+            },
+            Err(e) => {
+                error!("パケットの取得に失敗しました: {:?}", e);
+                // エラーが発生しても継続
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            },
         }
-
-        Ok(())
     }
 }
