12683da5ae23f7216ee905d05e4f1034ff024319
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-12-02 14:40:06 +0900

    ARPパケット解析コードのコメントアウトを解除

--- src/packet/analysis/analyzer.rs
+++ src/packet/analysis/analyzer.rs
@@ -79,7 +79,6 @@ impl PacketAnalyzer {
             return AnalyzeResult::Reject;
         }
 
-        /* // ARPパケット
         if ethernet_header.ether_type == EtherType::ARP {
             return match parse_arp_packet(ethernet_frame).await {
                 Ok(Some(marked_frame)) => AnalyzeResult::Accept(PacketData {
@@ -102,7 +101,7 @@ impl PacketAnalyzer {
         let mut checker = DUPLICATE_CHECKER.lock().await;
         if let Some(result) = checker.check_packet(ethernet_frame) {
             return result;
-        }*/
+        }
 
         // info!("通過パケット: src={}, dst={}, protocol={:?}", src_ip, dst_ip, ip_protocol);
 
